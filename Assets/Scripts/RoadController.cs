39f051bd5d6c5e57a50eb509588ad761b77216f8
jojunhyuung <cka830611@naver.com>
2025-01-23 11:41:52 +0900

    유니티 개발

--- Assets/Scripts/RoadController.cs
+++ Assets/Scripts/RoadController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoldController : MonoBehaviour
+public class RoadController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
