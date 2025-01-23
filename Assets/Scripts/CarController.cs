c4e83fa31de7a9155b48d3de2bd0742c146e3a62
jojunhyuung <cka830611@naver.com>
2025-01-23 17:09:29 +0900

    01-23 유니티

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int gas = 100;
+    [SerializeField] private float moveSpeed = 1f;
+    
+    public int Gas { get => gas; } // Gas 정보
+
+    private void Start()
     {
-        
+        StartCoroutine(GasCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GasCoroutine()
     {
-        
+        while (true)
+        {
+            gas -= 10;
+            if (gas <= 0) break;
+            yield return new WaitForSeconds(1f); 
+        }
+        // TODO: 게임 종료
+    }
+    
+    // 자동차 이름 메서드
+    public void Move(float direction)
+    {
+        transform.Translate(Vector3.right * direction * Time.deltaTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2f, 2f), 0, transform.position.z);
+    }
+    
+    // 가스 아이템 흭득시 호출되는 메서드
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Gas"))
+        {
+            gas += 30;
+            
+            //TODO: 가스 아이템 제거;
+        }
     }
 }
--- Assets/Scripts/RoadController.cs
+++ Assets/Scripts/RoadController.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoadController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 플레이어 차량이 도로에 진입하면 다음 도로를 생성
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.SpawnRoad(transform.position + new Vector3(0, 0, 10));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // 플레이어 차량이 도로를 벗어나면 해당 도로를 제거
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.DestroyRoad(gameObject);
+        }
     }
+
+    
 }
