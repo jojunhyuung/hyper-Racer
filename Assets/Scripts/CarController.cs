f965108e9d293af2d0b0fb2677117f3775f8ba38
jojunhyuung <cka830611@naver.com>
2025-01-24 13:12:26 +0900

    HR-8- 약간의 버그 수정

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -20,8 +20,9 @@ public class CarController : MonoBehaviour
         while (true)
         {
             gas -= 10;
+            yield return new WaitForSeconds(1f);
             if (gas <= 0) break;
-            yield return new WaitForSeconds(1f); 
+            
         }
         // 게임 종료
         GameManager.Instance.EndGame();
